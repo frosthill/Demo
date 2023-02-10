e7a5eb1d0ee9c515d5a5f2cbb6cecee9ce78ee31
Alexander Skvortcov <askvortcov@gmail.com>
2023-02-10 21:20:48 +0100

    initial build

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{width: '460px', margin: 'auto'}}>
+          <img src="https://avatars.githubusercontent.com/u/121947399" alt="logo" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

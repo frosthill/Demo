2f5a065abf2349144f3464948eb518723f11e9e5
Alexander Skvortcov <askvortcov@gmail.com>
2023-02-10 21:58:23 +0100

    add prettier and editorconfig
    yarn add --dev --exact prettier
    ...
    yarn add --dev eslint-config-prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{width: '460px', margin: 'auto'}}>
-          <img src="https://avatars.githubusercontent.com/u/121947399" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <div style={{ width: '460px', margin: 'auto' }}>
+          <img
+            src='https://avatars.githubusercontent.com/u/121947399'
+            alt='logo'
+          />
         </div>
       </header>
     </div>

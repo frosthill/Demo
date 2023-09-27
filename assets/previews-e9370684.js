47bd2ac5594daa682a42e692a637583ac3c760d7
askvortcov <github@softec.be>
2023-09-27 19:43:16 +0000

    Deploying to gh-pages from @ frosthill/ui@ddd6b59e896d284088bb2411ab2bc8ceded07d7b 🚀

--- assets/previews-e9370684.js
+++
@@ -1,1 +0,0 @@
-import{j as e,P as a,C as r,a as t,V as n,r as s,b as o}from"./index-7b19afa4.js";const x=()=>e.jsx(a,{children:e.jsx(r,{name:"App",children:e.jsx(t,{name:"Loader",children:e.jsx(n,{children:e.jsx(i,{})})})})});function i(){return e.jsx(s.Fragment,{children:"Loading..."})}const j=()=>e.jsx(o,{palette:e.jsx(x,{})});export{j as default};
